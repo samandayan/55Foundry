29b7520b9f06664b26ccb3787753dbd36562c0ae
Sam Dayan <saman_dayan@hotmail.com>
2019-12-27 13:03:03 -0800

    Initialized App

---
+++ app/src/main/java/com/sam/a55foundry/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sam.a55foundry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
